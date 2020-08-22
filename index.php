b55dd47e5ad2f78fb63bce5ceb266513a6d5f999
Abolarin Anuoluwapo <abolarin.aanu@gmail.com>
2020-08-22 19:05:57 +0100

    Modified Index

--- index.php
+++ index.php
@@ -5,7 +5,6 @@
 <?php require_once('includes/sidebar.php')?>
     
     
-
   <!-- Content Wrapper. Contains page content -->
   <div class="content-wrapper">
     <!-- Content Header (Page header) -->
